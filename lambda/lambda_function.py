e59b81ed69f5fbacc8fdb5d6a6797656ee24564d
Kumada <kumada.ren@stu.kist.ac.jp>
2026-09-21 20:45:28 +0900

    feat: add lambda source code for finsight-cost-api to version control

---
+++ lambda/lambda_function.py
@@ -0,0 +1,31 @@
+import boto3
+from datetime import date
+
+
+def lambda_handler(event, context):
+
+    client = boto3.client("ce")
+
+    today = date.today()
+
+    start_date = today.replace(day=1).isoformat()
+    end_date = today.isoformat()
+
+    response = client.get_cost_and_usage(
+        TimePeriod={
+            "Start": start_date,
+            "End": end_date
+        },
+        Granularity="MONTHLY",
+        Metrics=["UnblendedCost"]
+    )
+
+    cost = response["ResultsByTime"][0]["Total"]["UnblendedCost"]
+
+    return {
+        "statusCode": 200,
+        "body": {
+            "monthlyCost": cost["Amount"],
+            "unit": cost["Unit"]
+        }
+    }
